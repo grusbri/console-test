99de05eefb86d65a43e39e211df1be2c2b119433
rokrbro <gruspe.brian@gmail.com>
2021-06-23 10:01:32 +1000

    Force color

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "github.com/fatih/color"
 func main() {
 	// Print with default helper functions
 	// Create a new color object
-	c := color.New(color.FgCyan).Add(color.Underline)
+	color.NoColor = false
+	c := color.New(color.FgCyan)
 	c.Println("👌👍👋 Prints cyan text with an underline.")
 }
